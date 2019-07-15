974cc2f996572671e8662511e2289449d2d4c109
Tae Hee Choi <taeheechoi@Taes-iMac.local>
2019-07-14 22:39:58 -0700

    anagram, reverse-string

---
+++ coding-practice/anagram.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+def anagram(str1, str2):
+    return True if sorted([s for s in str1.strip()]) == sorted([s for s in str2.strip()]) else False
+
+class UnitTest(unittest.TestCase):
+    def test(self):
+        self.assertTrue(anagram('listen', 'silent'))
+        self.assertFalse(anagram('abc', 'def'))
+
+if __name__ == '__main__':
+    unittest.main()
